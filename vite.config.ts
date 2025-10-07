a5dd56e450d527e0ca6ef9c874d22ddb777a93ea
ALambot <22518006+ALambot@users.noreply.github.com>
2025-10-08 00:58:19 +0200

    Deployment on GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss()
   ],
+  base: '/grid-congestion-game/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
